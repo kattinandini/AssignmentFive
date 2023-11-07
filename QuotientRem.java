99eabc5890adf0bdfbfc422fe170251ce77568fd
kattinandini <kathinandhini49@gmail.com>
2023-11-07 19:08:30 +0530

    added QuotientRem.java

---
+++ QuotientRem.java
@@ -0,0 +1,12 @@
+package com.Bridgelab.assignmentfive;
+
+public class QuotientRem {
+    public static void main(String[] args) {
+        int a=4;
+        int b=2;
+        int rem=a%b;
+        int quo=a/b;
+        System.out.println("remainder = " + rem);
+        System.out.println("Quotient = " + quo);
+    }
+}
