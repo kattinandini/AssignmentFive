61a0a6a5501b6a3fe890ac2f3dd5c6c485a12e7d
kattinandini <kathinandhini49@gmail.com>
2023-11-07 19:12:24 +0530

    added SwapNum.java

---
+++ SwapNum.java
@@ -0,0 +1,15 @@
+package com.Bridgelab.assignmentfive;
+
+public class SwapNum {
+    public static void main(String[] args) {
+        int a = 4;
+        int b=6;
+        System.out.println("before swaping........ a = " + a + " b = "+ b);
+        int temp=a;
+
+        a=b;
+        b=temp;
+        System.out.println("After swaping........ a = " + a + " b = "+ b);
+
+    }
+}
